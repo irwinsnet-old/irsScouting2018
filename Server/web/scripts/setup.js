a6b6a076904758a7c51c134b3ce5fa94fbcbc466
stacy Irwin <stacy.irwin@outlook.com>
2017-12-17 08:25:16 -0800

    Deleted all files except for those used to host webpages on github.

--- Server/web/scripts/setup.js
+++
@@ -1,8 +0,0 @@
-function setEvent() {
-					var event = document.getElementById('event').value;
-					window.location = "/eventcurrent?event=" + event;
-}
-function getEvent() {
-					var event = document.getElementById('event').value;
-					window.location = "/eventfind?event=" + event;
-}
--- server.bat
+++
@@ -1,2 +0,0 @@
-cd Server
-python scoutingapi.py
